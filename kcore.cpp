fe80a24a57875b3aa5b487aeb160d5850911fee3
Paul O'Neil <redballoon36@gmail.com>
2012-12-05 15:27:47 +0000

    add kcore comparision, no termination yet

--- kcore.cpp
+++ kcore.cpp
@@ -6,16 +6,45 @@ using namespace graphlab;
 using namespace std;
 #include <unistd.h>
 
+#define KCORE_DEGREE	2
+
 struct vertex {
 	bool in_core;
-	
-	vertex() : in_core(true) { }
+	int changed;
+
+	vertex() : in_core(true), changed(0) { }
 	
 	void save(oarchive& oarc) const { }
 	void load(iarchive& iarc) { }
 };
 
 typedef distributed_graph<vertex, empty> graph_type;
+
+class kcore_program : public ivertex_program<graph_type, int>, public IS_POD_TYPE {
+public:
+	edge_dir_type gather_edges(icontext_type& context, const vertex_type& vertex) const {
+		return IN_EDGES;
+	}
+
+	int gather(icontext_type& context, const vertex_type& vertex, edge_type& edge) const {
+		return ( edge.source().data().in_core ? 1 : 0 );
+	}
+
+	void apply(icontext_type& context, vertex_type& vertex, const gather_type& total) {
+		if( total <  KCORE_DEGREE ) {
+			in_core = false;
+			changed += 1;
+		}
+	}
+
+	edge_dir_type scatter_edges(icontext_type& context, const vertex_type& vertex) const {
+		if( changed == 1 ) {
+		}
+		else {
+			return NO_EDGES;
+		}
+	}
+};
 
 class graph_writer
 {
@@ -44,6 +73,10 @@ int main(int argc, char* argv[])
 	graph.load_format("hdfs://ip-10-149-6-179/paul/graph.txt", "adj");
 
 	dc.cout() << "Hello World from " << dc.procid() << "( " << mpi_tools::rank() << ") / " << mpi_tools::size() << "!\n";
+
+	omni_engine<kcore_program> engine(dc, graph, "sync");
+	engine.signal_all();
+	engine.start();
 
 	graph.save("hdfs://ip-10-149-6-179/paul/output.txt", graph_writer(), false, true, false);
 
