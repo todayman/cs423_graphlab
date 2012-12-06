4e7053bcd402706002da9815bd5b46fe4447f85f
Paul O'Neil <redballoon36@gmail.com>
2012-12-06 00:47:14 +0000

    print progress during transfers

--- distribute.sh
+++ distribute.sh
@@ -3,7 +3,7 @@
 for dest in `cat ~/remote_machines`
 do
 	echo "Transfering to $dest"
-	rsync -az ~/graphlabapi/apps/paul_kcore $dest:~/graphlabapi/apps/
+	rsync -azP ~/graphlabapi/apps/paul_kcore $dest:~/graphlabapi/apps/
 	#echo "Building"
 	#ssh $dest "cd graphlabapi; ./configure; cd debug/apps/paul_kcore; make; cd ../../../release/apps/paul_kcore; make;" &
 done
